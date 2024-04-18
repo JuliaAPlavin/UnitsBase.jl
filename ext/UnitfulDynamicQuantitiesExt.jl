00000000fbc68bf9b1cdcc587ce4884ca3214c85
Alexander Plavin <alexander@plav.in>
2024-04-18 00:00:00 +0000

    improve; test type stability

--- ext/UnitfulDynamicQuantitiesExt.jl
+++ ext/UnitfulDynamicQuantitiesExt.jl
@@ -4,14 +4,18 @@ using Unitful
 using DynamicQuantities
 import UnitsBase: ustrip
 
-ustrip(u::Unitful.Units, x::DynamicQuantities.Quantity) = Unitful.ustrip(u, convert(Unitful.Quantity, DynamicQuantities.uexpand(x)))
-ustrip(u::DynamicQuantities.Quantity, x::Unitful.Quantity) = DynamicQuantities.ustrip(convert(DynamicQuantities.Quantity, x) |> u)
+ustrip(u::Unitful.Units, x::DynamicQuantities.Quantity) =
+    DynamicQuantities.ustrip(x / convert(DynamicQuantities.Quantity, 1.0*u))
+ustrip(u::Unitful.Units, x::DynamicQuantities.Quantity{<:Number, <:DynamicQuantities.AbstractSymbolicDimensions}) =
+    ustrip(u, DynamicQuantities.uexpand(x))
+ustrip(u::DynamicQuantities.Quantity, x::Unitful.Quantity) =
+    DynamicQuantities.ustrip(convert(DynamicQuantities.Quantity, x) |> u)
 
 Unitful.Quantity{T, D, U}(v::DynamicQuantities.Quantity) where {T,D,U} = convert(Unitful.Quantity{T, D, U}, v)
 
-Base.:*(x::DynamicQuantities.Quantity, y::Unitful.Quantity) = convert(Unitful.Quantity, x) * y
-Base.:*(x::Unitful.Quantity, y::DynamicQuantities.Quantity) = x * convert(Unitful.Quantity, y)
-Base.:/(x::DynamicQuantities.Quantity, y::Unitful.Quantity) = convert(Unitful.Quantity, x) / y
-Base.:/(x::Unitful.Quantity, y::DynamicQuantities.Quantity) = x / convert(Unitful.Quantity, y)
+for f in (:+, :-, :*, :/)
+    @eval Base.$f(x::DynamicQuantities.Quantity, y::Unitful.Quantity) = $f(x, convert(DynamicQuantities.Quantity, y))
+    @eval Base.$f(x::Unitful.Quantity, y::DynamicQuantities.Quantity) = $f(convert(DynamicQuantities.Quantity, x), y)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,24 +8,34 @@ using TestItemRunner
     import DynamicQuantities
 
     ux = 10.0*Unitful.u"mm/s"
-    dqx = 10.0*DynamicQuantities.us"mm/s"
+    dqx = 10.0*DynamicQuantities.u"mm/s"
+    dqsx = 10.0*DynamicQuantities.us"mm/s"
 
-    @test ustrip(Unitful.u"km/hr", ux) === 0.036
-    @test_broken ustrip(Unitful.u"km/hr", dqx) === 0.036
-    @test ustrip(Unitful.u"km/hr", dqx) ≈ 0.036
-    @test ustrip(DynamicQuantities.us"km/hr", ux) === 0.036
-    @test ustrip(DynamicQuantities.us"km/hr", dqx) === 0.036
-    @test_throws r"You can .*convert.*SymbolicDimensions" ustrip(DynamicQuantities.u"km/hr", dqx) === 0.036
-    @test ustrip(ux) === ustrip(dqx) === 10.
+    @test (@inferred ustrip(Unitful.u"km/hr", ux)) === 0.036
+    @test (@inferred ustrip(Unitful.u"km/hr", dqsx)) === 0.036
+    @test (@inferred ustrip(Unitful.u"km/hr", dqx)) === 0.036
+
+    @test (@inferred ustrip(DynamicQuantities.us"km/hr", ux)) === 0.036
+    @test (@inferred ustrip(DynamicQuantities.us"km/hr", dqsx)) === 0.036
+    @test (@inferred ustrip(DynamicQuantities.us"km/hr", dqx)) === 0.036
+    @test_throws r"You can .*convert.*SymbolicDimensions" ustrip(DynamicQuantities.u"km/hr", dqsx) === 0.036
+
+    @test ustrip(ux) === ustrip(dqsx) === 10.
     @test string(unit(ux)) == "mm s⁻¹"
-    @test string(unit(dqx)) == "s⁻¹ mm"
+    @test string(unit(dqsx)) == "s⁻¹ mm"
+    @test ustrip(dqx) == 0.01
+    @test string(unit(dqx)) == "m s⁻¹"
 
+    
     fu(x) = ustrip(Unitful.u"g/mm", x / 1Unitful.u"km")
     fdq(x) = ustrip(DynamicQuantities.us"g/mm", x / 1DynamicQuantities.u"km")
-    @test fu(1.0*Unitful.u"g") == 1e-6
-    @test fdq(1*Unitful.u"g") == 1e-6
-    @test fu(1*DynamicQuantities.u"g") == 1e-6
-    @test fdq(1.0*DynamicQuantities.u"g") == 1e-6
+    
+    @test (@inferred fu(1.0*Unitful.u"g")) == 1e-6
+    @test (@inferred fdq(1.0*DynamicQuantities.u"g")) == 1e-6
+
+    @test (@inferred fdq(1.0*Unitful.u"g")) == 1e-6
+    @test (@inferred fu(1*DynamicQuantities.u"g")) == 1e-6
+    @test (@inferred fu(1*DynamicQuantities.us"g")) == 1e-6
 end
 
 
